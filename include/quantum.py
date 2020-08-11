911cefcfbc4bdbd28015ab6088efed5914e4753c
user <abuzer@lu.lv>
2020-08-11 11:42:39 +0200

    major revisions on Grover's Search Implementation

--- include/quantum.py
+++ include/quantum.py
@@ -29,81 +29,10 @@ def angle_quantum_state(x,y):
 	if y<0: angle_degree = 360-angle_degree # degree of the angle
 	# else degree of the angle is the same as degree of the angle with state |0>
 	return angle_degree	
-	
-def oracle1(circuit,quantum_reg,number):
-    # prepare ancilla qubit
-    # circuit.x(quantum_reg[2])
-    # circuit.h(quantum_reg[2])
-    if(number%2 == 0):
-        circuit.x(quantum_reg[0])
-    if(number < 2):
-        circuit.x(quantum_reg[1])
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[2])
-    if(number < 2):
-        circuit.x(quantum_reg[1])
-    if(number%2 == 0):
-        circuit.x(quantum_reg[0])
-    # put ancilla qubit back into state |0>
-    # circuit.h(quantum_reg[2])
-    # circuit.x(quantum_reg[2])
 
-def oracle2(circuit,quantum_reg,number):
-    # prepare ancilla qubit
-    circuit.x(quantum_reg[4])
-    circuit.h(quantum_reg[4])
 
-    if(number%2 == 0):
-        circuit.x(quantum_reg[0])
-    if(number%4 < 2):
-        circuit.x(quantum_reg[1])
-    if(number < 4):
-        circuit.x(quantum_reg[2])
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[3])
-    circuit.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[4])
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[3])
-    if(number < 4):
-        circuit.x(quantum_reg[2])
-    if(number%4 < 2):
-        circuit.x(quantum_reg[1])
-    if(number%2 == 0):
-        circuit.x(quantum_reg[0])
-
-    # put ancilla qubit back into state |0>
-    circuit.h(quantum_reg[4])
-    circuit.x(quantum_reg[4])
-
-def oracle3(circuit,quantum_reg,number):
-    # prepare ancilla qubit
-    circuit.x(quantum_reg[6])
-    circuit.h(quantum_reg[6])
-
-    if(number%2 == 0):
-        circuit.x(quantum_reg[0])
-    if(number%4 < 2):
-        circuit.x(quantum_reg[1])
-    if(number%8 < 4):
-        circuit.x(quantum_reg[2])
-    if(number < 8):
-        circuit.x(quantum_reg[3])
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[4])
-    circuit.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[5])
-    circuit.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[6])
-    circuit.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[5])
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[4])
-    if(number < 8):
-        circuit.x(quantum_reg[3])
-    if(number%8 < 4):
-        circuit.x(quantum_reg[2])
-    if(number%4 < 2):
-        circuit.x(quantum_reg[1])
-    if(number%2 == 0):
-        circuit.x(quantum_reg[0])
-
-    # put ancilla qubit back into state |0>
-    circuit.h(quantum_reg[6])
-    circuit.x(quantum_reg[6])
-
-def giant_oracle(circuit99,quantum_reg,number):
+def giant_oracle(circuit99,quantum_reg):
+    number=18
     if(number%2 == 0):
         circuit99.x(quantum_reg[0])
     if(number%4 < 2):
@@ -125,27 +54,27 @@ def giant_oracle(circuit99,quantum_reg,number):
     if(number < 512):
         circuit99.x(quantum_reg[9])
     
-    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[10])
+    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[18])
     circuit99.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[11])
     circuit99.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[12])
     circuit99.ccx(quantum_reg[6],quantum_reg[7],quantum_reg[13])
     circuit99.ccx(quantum_reg[8],quantum_reg[9],quantum_reg[14])
     
-    circuit99.ccx(quantum_reg[10],quantum_reg[11],quantum_reg[15])
+    circuit99.ccx(quantum_reg[18],quantum_reg[11],quantum_reg[15])
     circuit99.ccx(quantum_reg[12],quantum_reg[13],quantum_reg[16])
     
     circuit99.ccx(quantum_reg[15],quantum_reg[16],quantum_reg[17])
-    circuit99.ccx(quantum_reg[14],quantum_reg[17],quantum_reg[18])
+    circuit99.ccx(quantum_reg[14],quantum_reg[17],quantum_reg[10])
     circuit99.ccx(quantum_reg[15],quantum_reg[16],quantum_reg[17])
     
     circuit99.ccx(quantum_reg[12],quantum_reg[13],quantum_reg[16])
-    circuit99.ccx(quantum_reg[10],quantum_reg[11],quantum_reg[15])
+    circuit99.ccx(quantum_reg[18],quantum_reg[11],quantum_reg[15])
     
     circuit99.ccx(quantum_reg[8],quantum_reg[9],quantum_reg[14])
     circuit99.ccx(quantum_reg[6],quantum_reg[7],quantum_reg[13])
     circuit99.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[12])
     circuit99.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[11])
-    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[10])
+    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[18])
     
     if(number%2 == 0):
         circuit99.x(quantum_reg[0])
@@ -168,106 +97,114 @@ def giant_oracle(circuit99,quantum_reg,number):
     if(number < 512):
         circuit99.x(quantum_reg[9])
 
-def diffusion1(circuit,quantum_reg):
-    #step 1
-    # circuit.x(quantum_reg[2])
-    # circuit.h(quantum_reg[2])
-    
-    #step 2
-    circuit.h(quantum_reg[1])
-    circuit.h(quantum_reg[0])
-    circuit.x(quantum_reg[1])
-    circuit.x(quantum_reg[0])
+def giant_oracle2(circuit99,quantum_reg):
+    numbers=[12,45]
+    for number in numbers:
+        if(number%2 == 0):
+            circuit99.x(quantum_reg[0])
+        if(number%4 < 2):
+            circuit99.x(quantum_reg[1])
+        if(number%8 < 4):
+            circuit99.x(quantum_reg[2])
+        if(number%16 < 8):
+            circuit99.x(quantum_reg[3])
+        if(number%32 < 16):
+            circuit99.x(quantum_reg[4])
+        if(number%64 < 32):
+            circuit99.x(quantum_reg[5])
+        if(number%128 < 64):
+            circuit99.x(quantum_reg[6])
+        if(number%256 < 128):
+            circuit99.x(quantum_reg[7])
+        if(number%512 < 256):
+            circuit99.x(quantum_reg[8])
+        if(number < 512):
+            circuit99.x(quantum_reg[9])
 
-    #step 3
-    circuit.ccx(quantum_reg[1],quantum_reg[0],quantum_reg[2])
+        circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[18])
+        circuit99.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[11])
+        circuit99.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[12])
+        circuit99.ccx(quantum_reg[6],quantum_reg[7],quantum_reg[13])
+        circuit99.ccx(quantum_reg[8],quantum_reg[9],quantum_reg[14])
 
-    #step 4
-    circuit.x(quantum_reg[2])
+        circuit99.ccx(quantum_reg[18],quantum_reg[11],quantum_reg[15])
+        circuit99.ccx(quantum_reg[12],quantum_reg[13],quantum_reg[16])
 
-    #step 5
-    circuit.x(quantum_reg[1])
-    circuit.x(quantum_reg[0])
-    circuit.h(quantum_reg[1])
-    circuit.h(quantum_reg[0])
+        circuit99.ccx(quantum_reg[15],quantum_reg[16],quantum_reg[17])
+        circuit99.ccx(quantum_reg[14],quantum_reg[17],quantum_reg[10])
+        circuit99.ccx(quantum_reg[15],quantum_reg[16],quantum_reg[17])
 
-    #step 6
-    # circuit.h(quantum_reg[2])
-    # circuit.x(quantum_reg[2])
+        circuit99.ccx(quantum_reg[12],quantum_reg[13],quantum_reg[16])
+        circuit99.ccx(quantum_reg[18],quantum_reg[11],quantum_reg[15])
 
-def diffusion2(circuit,quantum_reg):
-    circuit.x(quantum_reg[4])
-    circuit.h(quantum_reg[4])
+        circuit99.ccx(quantum_reg[8],quantum_reg[9],quantum_reg[14])
+        circuit99.ccx(quantum_reg[6],quantum_reg[7],quantum_reg[13])
+        circuit99.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[12])
+        circuit99.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[11])
+        circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[18])
 
-    for i in range(3):
-        circuit.h(quantum_reg[i])
-        circuit.x(quantum_reg[i])
+        if(number%2 == 0):
+            circuit99.x(quantum_reg[0])
+        if(number%4 < 2):
+            circuit99.x(quantum_reg[1])
+        if(number%8 < 4):
+            circuit99.x(quantum_reg[2])
+        if(number%16 < 8):
+            circuit99.x(quantum_reg[3])
+        if(number%32 < 16):
+            circuit99.x(quantum_reg[4])
+        if(number%64 < 32):
+            circuit99.x(quantum_reg[5])
+        if(number%128 < 64):
+            circuit99.x(quantum_reg[6])
+        if(number%256 < 128):
+            circuit99.x(quantum_reg[7])
+        if(number%512 < 256):
+            circuit99.x(quantum_reg[8])
+        if(number < 512):
+            circuit99.x(quantum_reg[9])
 
-    circuit.ccx(quantum_reg[1],quantum_reg[0],quantum_reg[3])
-    circuit.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[4])
-    circuit.ccx(quantum_reg[1],quantum_reg[0],quantum_reg[3])
-
-    circuit.x(quantum_reg[4])
-
-    for i in range(3):
-        circuit.x(quantum_reg[i])
-        circuit.h(quantum_reg[i])
-
-    circuit.h(quantum_reg[4])
-    circuit.x(quantum_reg[4])
-    
-def diffusion3(circuit,quantum_reg):
-    circuit.x(quantum_reg[6])
-    circuit.h(quantum_reg[6])
-
-    for i in range(4):
-        circuit.h(quantum_reg[i])
-        circuit.x(quantum_reg[i])
-
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[4])
-    circuit.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[5])
-    circuit.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[6])
-    circuit.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[5])
-    circuit.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[4])
-
-    circuit.x(quantum_reg[6])
-
-    for i in range(4):
-        circuit.x(quantum_reg[i])
-        circuit.h(quantum_reg[i])
-
-    circuit.h(quantum_reg[6])
-    circuit.x(quantum_reg[6])
-    
 def giant_diffusion(circuit99,quantum_reg):
     for i in range(10):
         circuit99.h(quantum_reg[i])
         circuit99.x(quantum_reg[i])
 
-    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[10])
+    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[18])
     circuit99.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[11])
     circuit99.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[12])
     circuit99.ccx(quantum_reg[6],quantum_reg[7],quantum_reg[13])
     circuit99.ccx(quantum_reg[8],quantum_reg[9],quantum_reg[14])
     
-    circuit99.ccx(quantum_reg[10],quantum_reg[11],quantum_reg[15])
+    circuit99.ccx(quantum_reg[18],quantum_reg[11],quantum_reg[15])
     circuit99.ccx(quantum_reg[12],quantum_reg[13],quantum_reg[16])
     
     circuit99.ccx(quantum_reg[15],quantum_reg[16],quantum_reg[17])
-    circuit99.ccx(quantum_reg[14],quantum_reg[17],quantum_reg[18])
+    circuit99.ccx(quantum_reg[14],quantum_reg[17],quantum_reg[10])
     circuit99.ccx(quantum_reg[15],quantum_reg[16],quantum_reg[17])
     
     circuit99.ccx(quantum_reg[12],quantum_reg[13],quantum_reg[16])
-    circuit99.ccx(quantum_reg[10],quantum_reg[11],quantum_reg[15])
+    circuit99.ccx(quantum_reg[18],quantum_reg[11],quantum_reg[15])
     
     circuit99.ccx(quantum_reg[8],quantum_reg[9],quantum_reg[14])
     circuit99.ccx(quantum_reg[6],quantum_reg[7],quantum_reg[13])
     circuit99.ccx(quantum_reg[4],quantum_reg[5],quantum_reg[12])
     circuit99.ccx(quantum_reg[2],quantum_reg[3],quantum_reg[11])
-    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[10])
+    circuit99.ccx(quantum_reg[0],quantum_reg[1],quantum_reg[18])
 
     for i in range(10):
         circuit99.x(quantum_reg[i])
         circuit99.h(quantum_reg[i])
     
-    circuit99.x(quantum_reg[18])
+    circuit99.x(quantum_reg[10])
+
+
+def Uf(circuit,qreg):
+	circuit.ccx(qreg[0],qreg[1],qreg[2])
+	
+def Uf_8(circuit,quantum_reg):
+	circuit.x(quantum_reg[2])
+	circuit.ccx(quantum_reg[2],quantum_reg[1],quantum_reg[4])
+	circuit.ccx(quantum_reg[4],quantum_reg[0],quantum_reg[3])
+	circuit.ccx(quantum_reg[2],quantum_reg[1],quantum_reg[4])
+	circuit.x(quantum_reg[2])
+	
